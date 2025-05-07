f6a6a5ed04338e95ccec9a852e4712d2db4cced7
Laike-Endaril <silvertallon@gmail.com>
2025-05-07 14:44:30 -0700

    1.12.2.000a

--- src/main/java/com/fantasticsource/customparticles/CustomParticles.java
+++ src/main/java/com/fantasticsource/customparticles/CustomParticles.java
@@ -10,12 +10,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = CustomParticles.MODID, name = CustomParticles.NAME, version = CustomParticles.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.058,)")
+@Mod(modid = CustomParticles.MODID, name = CustomParticles.NAME, version = CustomParticles.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.058a,)")
 public class CustomParticles
 {
     public static final String MODID = "customparticles";
     public static final String NAME = "Custom Particles";
-    public static final String VERSION = "1.12.2.000";
+    public static final String VERSION = "1.12.2.000a";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
