2bc0b8b1af2f85c30c01574ebe7db9ea92e78822
Laike-Endaril <silvertallon@gmail.com>
2025-05-09 18:29:22 -0700

    Updated lib and req

--- src/main/java/com/fantasticsource/customparticles/CustomParticles.java
+++ src/main/java/com/fantasticsource/customparticles/CustomParticles.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = CustomParticles.MODID, name = CustomParticles.NAME, version = CustomParticles.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.059,)")
+@Mod(modid = CustomParticles.MODID, name = CustomParticles.NAME, version = CustomParticles.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.060,)")
 public class CustomParticles
 {
     public static final String MODID = "customparticles";
