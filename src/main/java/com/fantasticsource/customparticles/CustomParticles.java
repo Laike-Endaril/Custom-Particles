e5ade19371acc50259bcfb23c9ddcc025b940f20
Laike-Endaril <silvertallon@gmail.com>
2025-05-26 09:27:39 -0700

    Updated lib and req (has yaw calc fixes)

--- src/main/java/com/fantasticsource/customparticles/CustomParticles.java
+++ src/main/java/com/fantasticsource/customparticles/CustomParticles.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = CustomParticles.MODID, name = CustomParticles.NAME, version = CustomParticles.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.061,)")
+@Mod(modid = CustomParticles.MODID, name = CustomParticles.NAME, version = CustomParticles.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.063,)")
 public class CustomParticles
 {
     public static final String MODID = "customparticles";
