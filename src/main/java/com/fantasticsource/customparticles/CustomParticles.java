6135a786250887a994adad98fea0be0f692d9aff
Laike-Endaril <silvertallon@gmail.com>
2025-07-24 08:49:50 -0600

    1.12.2.004

--- src/main/java/com/fantasticsource/customparticles/CustomParticles.java
+++ src/main/java/com/fantasticsource/customparticles/CustomParticles.java
@@ -15,7 +15,7 @@ public class CustomParticles
 {
     public static final String MODID = "customparticles";
     public static final String NAME = "Custom Particles";
-    public static final String VERSION = "1.12.2.003";
+    public static final String VERSION = "1.12.2.004";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
